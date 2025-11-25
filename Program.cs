cf6f5bf2563b71ed37d768f4fdc314827f08e7d4
Ben Stolovitz <ben@stolovitz.com>
2025-11-25 10:39:39 -0500

    clearer logs...

--- Program.cs
+++ Program.cs
@@ -3,11 +3,30 @@ using static Crayon.Output;
 
 using Helpers;
 
-var query = new EventLogQuery("System", PathType.LogName);
+// var queryString = @"
+//   *[System[(Level=1 or Level=2 or Level=3) and TimeCreated[timediff(@SystemTime) <= 86400000]]]
+// ";
+
+// or @Name='EventLog'
+// or @Name='Microsoft-Windows-Kernel-PnP'
+// or @Name='Microsoft-Windows-DriverFrameworks-UserMode'
+var queryString = @"
+  *[System/Provider[@Name='TPM-WMI'
+    or @Name='Kernel-General'
+    or @Name='Kernel-Power'
+    or @Name='User32'
+    or @Name='WindowsUpdateClient'
+    or @Name='WER-SystemErrorReporting'
+
+    or @Name='Microsoft-Windows-WindowsUpdateClient'
+    or @Name='Microsoft-Windows-UserPnp']]
+";
+
+var query = new EventLogQuery("System", PathType.LogName, queryString);
 query.ReverseDirection = true;
 
 var reader = new EventLogReader(query);
-foreach (var eventRecord in reader.Iterator().Take(10))
+foreach (var eventRecord in reader.Iterator().Take(50))
 {
-    Console.WriteLine($"Event ID: {eventRecord.Id}, Level: {eventRecord.LevelDisplayName}, TimeCreated: {eventRecord.TimeCreated}");
+    Console.WriteLine($"{Dim($"[{eventRecord.ProviderName}]")} {eventRecord.Id} {eventRecord.TaskDisplayName}, Level: {eventRecord.LevelDisplayName}, TimeCreated: {eventRecord.TimeCreated}");
 }
