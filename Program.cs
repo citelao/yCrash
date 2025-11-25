fd0cdc8f232fabafafe131c00d3ba9b9215972ff
Ben Stolovitz <ben@stolovitz.com>
2025-11-25 09:58:07 -0500

    log system events!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics.Eventing.Reader;
+
+using System.Diagnostics;
+
+var query = new EventLogQuery("System", PathType.LogName);
+query.ReverseDirection = true;
+
+var reader = new EventLogReader(query);
+
+var count = 0;
+while (count < 10)
+{
+    var eventRecord = reader.ReadEvent();
+    if (eventRecord == null)
+        break;
+
+    Console.WriteLine($"Event ID: {eventRecord.Id}, Level: {eventRecord.LevelDisplayName}, TimeCreated: {eventRecord.TimeCreated}");
+    count++;
+}
+
+// var reader = new EventLog
 Console.WriteLine("Hello, World!");
